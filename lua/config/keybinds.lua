9f211db617d093311a78ff3a5f1c3b2421db88e0
Joelorbit <abitieyuel@gmail.com>
2026-07-13 00:53:47 +0300

    Back to normal mode cause you cant write in heaven!

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -4,8 +4,8 @@ vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>w", "<cmd>w<CR>", { desc = "Save file" })
 vim.keymap.set("n", "<leader>q", "<cmd>q<CR>", { desc = "Quit" })
 vim.keymap.set("i", "jk", "<Esc>", { desc = "Exit insert mode" })
-vim.keymap.set("i", "<leader>w", "<C-o><cmd>w<CR>", { desc = "Save file while in insert mode" })
-vim.keymap.set("i", "<leader>q", "<C-o><cmd>q<CR>", { desc = "Quit file while in insert mode" })
+vim.keymap.set("n", "<leader>x", "<cmd>wq<CR>", { desc = "Save and quit while in normal mode" })
+
 
 vim.keymap.set("n", "<C-t>", "<cmd>ToggleTerm<CR>", { desc = "Toggle Terminal" })
 vim.keymap.set("t", "<C-t>", [[<C-\><C-n><cmd>ToggleTerm<CR>]], { desc = "Toggle Terminal" })
