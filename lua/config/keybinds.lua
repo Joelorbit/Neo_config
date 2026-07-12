3a0a9c734277b165a8c96cf2631bc05b6003c130
Joelorbit <abitieyuel@gmail.com>
2026-07-13 00:30:59 +0300

    Added toggle terminal in my config

--- lua/config/keybinds.lua
+++ lua/config/keybinds.lua
@@ -1,2 +1,11 @@
-vim.g.mapleader= " "
+vim.g.mapleader = " "
+
 vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>w", "<cmd>w<CR>", { desc = "Save file" })
+vim.keymap.set("n", "<leader>q", "<cmd>q<CR>", { desc = "Quit" })
+vim.keymap.set("i", "jk", "<Esc>", { desc = "Exit insert mode" })
+
+vim.keymap.set("n", "<C-t>", "<cmd>ToggleTerm<CR>", { desc = "Toggle Terminal" })
+vim.keymap.set("t", "<C-t>", [[<C-\><C-n><cmd>ToggleTerm<CR>]], { desc = "Toggle Terminal" })
+
+
---
+++ lua/plugins/toggleterm.lua
@@ -0,0 +1,11 @@
+return {
+  "akinsho/toggleterm.nvim",
+  version = "*",
+  opts = {
+    direction = "float",
+    shade_terminals = true,
+    float_opts = {
+      border = "rounded",
+    },
+  },
+}
